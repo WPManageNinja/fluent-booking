ea07e80e6904e0bd208fc823802c4cc8add8776e
Lukman Nakib <lukman.nakib@gmail.com>
2023-08-28 11:10:41 +0600

    Fix php 8.2 deprecation

--- vendor/wpfluent/framework/src/WPFluent/Foundation/Container.php
+++ vendor/wpfluent/framework/src/WPFluent/Foundation/Container.php
@@ -1161,6 +1161,8 @@ class Container implements ArrayAccess, ContainerContract
      * @param  string  $key
      * @return bool
      */
+    #[\ReturnTypeWillChange]
+    
     public function offsetExists($key)
     {
         return isset($this->bindings[$key]);
@@ -1172,6 +1174,8 @@ class Container implements ArrayAccess, ContainerContract
      * @param  string  $key
      * @return mixed
      */
+    #[\ReturnTypeWillChange]
+    
     public function offsetGet($key)
     {
         return $this->make($key);
@@ -1184,6 +1188,7 @@ class Container implements ArrayAccess, ContainerContract
      * @param  mixed   $value
      * @return void
      */
+    #[\ReturnTypeWillChange]
     public function offsetSet($key, $value)
     {
         // If the value is not a Closure, we will make it one. This simply gives
@@ -1204,6 +1209,8 @@ class Container implements ArrayAccess, ContainerContract
      * @param  string  $key
      * @return void
      */
+    #[\ReturnTypeWillChange]
+    
     public function offsetUnset($key)
     {
         unset($this->bindings[$key], $this->instances[$key], $this->resolved[$key]);
