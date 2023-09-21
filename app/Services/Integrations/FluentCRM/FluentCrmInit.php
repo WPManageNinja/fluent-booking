09fbd5a03fde82daebffd8e87de00c8042dec26d
Raiyan Marzan <raiyanmarzan.rm@gmail.com>
2023-09-21 10:17:31 +0600

    Completed Profile Section Integration

--- app/Services/Integrations/FluentCRM/FluentCrmInit.php
+++ app/Services/Integrations/FluentCRM/FluentCrmInit.php
@@ -2,9 +2,14 @@
 
 namespace FluentBooking\App\Services\Integrations\FluentCRM;
 
+use FluentBooking\App\Models\Booking;
+use FluentBooking\App\Models\CalendarSlot;
+use FluentBooking\App\Services\DateTimeHelper;
+
 class FluentCrmInit {
 
 	public function __construct() {
+        $this->registerHooks();
 		$this->registerIntegrations();
 	}
 
@@ -17,6 +22,13 @@ class FluentCrmInit {
 		$this->addContactMenuSection();
 		$this->addAutomations();
 	}
+
+    public function registerHooks()
+    {
+        add_action('fluent_booking/booking_schedule', [$this, 'addProfileLink'], 10, 1);
+        add_filter('fluentcrm_profile_sections', [$this, 'addProfileSection'], 10, 1);
+        add_filter('fluentcrm_get_form_submissions_fluent_booking', [$this, 'getScheduledMeetings'], 10, 2);      
+    }
 
 	/**
 	 * load Assets for to Fluent CRM  contact section
@@ -36,5 +48,103 @@ class FluentCrmInit {
 		 */
 	}
 
+    private function getSubscriberId($email)
+    {
+        $contact = FluentCrmApi('contacts')->getContact($email);
+        return $contact ? $contact->id : null;
+    }
 
+    public function addProfileLink(&$booking)
+    {
+        $subscriberId = $booking->person_user_id ?: $this->getSubscriberId($booking->email);
+
+        if (!$subscriberId) {
+            return;
+        }
+        
+        $url = admin_url('admin.php?page=fluentcrm-admin#/subscribers/' . $subscriberId);
+
+        $link = '<a target="_blank" href="' . esc_url($url) . '">' . 'profile' . '</a>';
+
+        $booking->crm_profile = $link;
+    }
+
+    public function addProfileSection($sections)
+    {
+        $sections['booking'] = [
+            'name'    => 'booking',
+            'title'   => __('Bookings', 'fluent-crm'),
+            'handler' => 'route'
+        ];
+
+        return $sections;
+    }
+
+    private function getActionUrl($meeting)
+    {
+        $url = admin_url('admin.php?page=fluent-booking#/scheduled-events?spot_id=' . $meeting->event_id);
+
+        $link = '<a target="_blank" href="' . esc_url($url) . '">' . 'view' . '</a>';
+        
+        return $link;
+    }
+
+    private function getFormattedTime($meeting)
+    {
+        $formattedTime = DateTimeHelper::convertToTimeZone($meeting->start_time, 'utc', $meeting->calendar->author_timezone, 'j M Y, g:i A');
+
+        return $formattedTime;
+    }
+
+    public function getScheduledMeetings($data, $subsriber)
+    {
+        $app      = fluentCrm();
+        $page     = intval($app->request->get('page', 1));
+        $perPage  = intval($app->request->get('per_page', 10));
+
+        $meetings = Booking::with(['slot', 'calendar'])
+            ->where('email', $subsriber->email)
+            ->distinct('event_id')
+            ->orderBy('start_time', 'DESC')
+            ->paginate();
+
+        $formattedMeetings = [];
+
+        foreach ($meetings->items() as $meeting){
+            $host = $meeting->calendar->getAuthorProfile();
+            $formattedMeetings[] = [
+                'id'           => '#'.$meeting->event_id,
+                'title'        => $meeting->slot->title . ' with ' . $host['name'],
+                'status'       => $meeting->status,
+                'meeting_at'   => $this->getFormattedTime($meeting),
+                'action'       => $this->getActionUrl($meeting)
+            ];
+        }
+
+        return [
+            'total' => $meetings->total(),
+            'data'  => $formattedMeetings,
+            'columns_config' => [
+                'id' => [
+                    'label' => 'ID',
+                    'width' => '100px'
+                ],
+                'title' => [
+                    'label' => 'Event',
+                ],
+                'status' => [
+                    'label' => 'Status',
+                    'width' => '150px'
+                ],
+                'meeting_at' => [
+                    'label' => 'Meeting At',
+                    'width' => '200px'
+                ],
+                'action' => [
+                    'label' => 'Action',
+                    'width' => '100px'
+                ]
+            ]
+        ];
+    }
 }
