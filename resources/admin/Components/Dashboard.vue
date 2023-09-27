8ca905ff24080d1dabce6293a0146460fe4b95d6
Tanbir <tanbirsylhet20@gmail.com>
2023-09-27 14:25:10 +0600

    Finished dashboard

--- resources/admin/Components/Dashboard.vue
+++ resources/admin/Components/Dashboard.vue
@@ -1,27 +1,141 @@
 <template>
-    <div class="dashboard box_wrapper">
-        <div class="box dashboard_box box_narrow">
-            <div class="box_header"
-                 style="padding: 35px;font-size: 16px;background: white;max-width: 600px;margin: 0 auto;">
-                Hello {{ me.full_name }}!
-                <p>You can replace this module from <code>/resources/admin/Components/Dashboard.vue</code></p>
-                <b>appVars</b>
-                <pre>{{ appVars }}</pre>
+    <div class="fcal_dashboard fcal_section fcal_section_narrow">
+        <div class="fcal_dashboard_overview fcal_dashboard_box">
+            <div class="fcal_section_header">
+                <div class="fcal_title">
+                    <h3>Overview</h3>
+                </div>
+                <div class="fcal_actions">
+                    <el-date-picker
+                        v-model="filterDate"
+                        type="daterange"
+                        unlink-panels
+                        range-separator="-"
+                        start-placeholder="Start date"
+                        end-placeholder="End date"
+                        :shortcuts="shortcuts"
+                        popper-class="fcal_daterange_popover"
+                    />
+                </div>
+            </div>
+
+            <el-row v-if="loading" :gutter="15">
+                <el-col :span="6">
+                    <el-skeleton :rows="3" animated />
+                </el-col>
+                <el-col :span="6">
+                    <el-skeleton :rows="3" animated />
+                </el-col>
+                <el-col :span="6">
+                    <el-skeleton :rows="3" animated />
+                </el-col>
+                <el-col :span="6">
+                    <el-skeleton :rows="3" animated />
+                </el-col>
+            </el-row>
+            <div v-else class="overview-widgets">
+                <div
+                    v-for="(widget, i) in widgets"
+                    :key="i"
+                    class="overview-widget">
+                    <h3>{{ widget.title }}</h3>
+                    <h1>{{ widget.number }}</h1>
+                    <p>{{ widget.content }}</p>
+                    <span class="stat"><el-icon><Top /></el-icon> {{ widget.stat }}</span>
+                    <span class="icon" v-html="widget.icon"></span>
+                </div>
             </div>
         </div>
+
+        <div class="fcal_dashboard_chat_wrap fcal_dashboard_box">
+            <div class="fcal_section_header">
+                <div class="fcal_title">
+                    <h3>Completed Bookings Trend</h3>
+                </div>
+                <div class="fcal_actions">
+                    <el-date-picker
+                        v-model="filterDate2"
+                        type="date"
+                        placeholder="Select Date"
+                        popper-class="fcal_daterange_popover"
+                    />
+                </div>
+            </div>
+            <el-skeleton v-if="loading" :rows="4" animated />
+            <div v-else class="fcal_dashboard_chat">
+                <ReportChat :data="chatData" :categories="chatCats" />
+            </div>
+        </div>
+
     </div>
 </template>
 
 <script type="text/babel">
+import { Top } from '@element-plus/icons-vue';
+import ReportChat from "../Pieces/_ReportChat";
+
 export default {
     name: 'Dashboard',
+    components: {
+        ReportChat,
+        Top
+    },
     data() {
         return {
-            me: this.appVars.me,
+            filterDate: '',
+            filterDate2: '',
+            shortcuts: [
+                {
+                    text: 'Last week',
+                    value: () => {
+                        const end = new Date()
+                        const start = new Date()
+                        start.setTime(start.getTime() - 3600 * 1000 * 24 * 7)
+                        return [start, end]
+                    },
+                },
+                {
+                    text: 'Last month',
+                    value: () => {
+                        const end = new Date()
+                        const start = new Date()
+                        start.setTime(start.getTime() - 3600 * 1000 * 24 * 30)
+                        return [start, end]
+                    },
+                },
+                {
+                    text: 'Last 3 months',
+                    value: () => {
+                        const end = new Date()
+                        const start = new Date()
+                        start.setTime(start.getTime() - 3600 * 1000 * 24 * 90)
+                        return [start, end]
+                    },
+                },
+            ],
+            loading: false,
+            widgets: '',
+            chatData: [30,40,35,50,49,60,70,91,125],
+            chatCats: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
+        }
+    },
+    methods: {
+        fetchReports() {
+            this.loading = true;
+            this.$get('reports')
+                .then(response => {
+                    this.widgets = response.overview;
+                })
+                .catch(errors => {
+                    this.$handleError(errors);
+                })
+                .finally(() => {
+                    this.loading = false;
+                });
         }
     },
     mounted() {
-        this.$router.push({ name: 'calendars' });
+        this.fetchReports();
     }
 };
 </script>
---
+++ resources/admin/Pieces/_ReportChat.vue
@@ -0,0 +1,49 @@
+<template>
+    <VueApexCharts :options="options" height="375" :series="options.series" />
+</template>
+
+<script>
+import VueApexCharts from "vue3-apexcharts";
+
+export default {
+    name: "_ReportChat",
+    props: ['data', 'categories'],
+    components: {
+        VueApexCharts
+    },
+    data() {
+        return {
+            options: {
+                chart: {
+                    type: 'area',
+                    toolbar: {
+                        show: false,
+                    }
+                },
+                dataLabels: {
+                    enabled: false,
+                },
+                grid: {
+                    borderColor: '#F3F3F3',
+                    yaxis: {
+                        lines: {
+                            show: true
+                        }
+                    },
+                },
+                series: [{
+                    name: 'sales',
+                    data: this.data,//[30,40,35,50,49,60,70,91,125]
+                }],
+                xaxis: {
+                    categories: this.categories//[1991,1992,1993,1994,1995,1996,1997, 1998,1999]
+                }
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
