a79656de8971cd040ac481c09caeb293d9650c99
Tanbir <tanbirsylhet20@gmail.com>
2023-10-04 19:00:37 +0600

    working on the Fluent Booking block.

---
+++ resources/Blocks/inspector-settings.js
@@ -0,0 +1,33 @@
+/*eslint-disable*/
+// const { InspectorControls } = wp.blockEditor;
+// const {__} = wp.i18n;
+// const {
+//     PanelBody,
+//     PanelRow
+// } = wp.components;
+
+
+const InspectorSettings = props => {
+    // const {
+    //     attributes: {
+    //         calenderId
+    //     }, setAttributes
+    // } = props;
+
+    return (
+        <>
+            <h1>Fluent-booking Settings</h1>
+
+            {/*<InspectorControls>*/}
+            {/*    <PanelBody title="General Settings"*/}
+            {/*               initialOpen={true}*/}
+            {/*    >*/}
+            {/*        <PanelRow>*/}
+            {/*            <h1>Fluent-booking Settings</h1>*/}
+            {/*        </PanelRow>*/}
+            {/*    </PanelBody>*/}
+            {/*</InspectorControls>*/}
+        </>
+    );
+};
+export default InspectorSettings;
