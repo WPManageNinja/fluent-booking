0b3cc3a5c6c830dd9de90dfa05f823f800306077
Shahjahan Jewel <cep.jewel@gmail.com>
2023-10-04 22:23:37 +0600

    Added Frontend Sharing

--- app/Services/LandingPage/LandingPageHandler.php
+++ app/Services/LandingPage/LandingPageHandler.php
@@ -3,16 +3,20 @@
 namespace FluentBooking\App\Services\LandingPage;
 
 use FluentBooking\App\App;
+use FluentBooking\App\Hooks\Handlers\FrontEndHandler;
+use FluentBooking\App\Models\Booking;
 use FluentBooking\App\Models\Calendar;
 use FluentBooking\App\Models\CalendarSlot;
+use FluentBooking\App\Services\BookingService;
 use FluentBooking\App\Services\Helper;
+use FluentBooking\Framework\Support\Arr;
 
 class LandingPageHandler
 {
     public function boot()
     {
         if (defined('FLUENT_BOOKING_LANDING_SLUG')) {
-            add_action('template_redirect', [$this, 'handleSlugDefinedPage']);
+            add_action('template_redirect', [$this, 'handleSlugDefinedPage'], 1);
         } else if (isset($_GET['fluent-booking']) && $_GET['fluent-booking'] == 'calendar') {
             add_action('init', [$this, 'handleUrlParamsPage']);
         }
@@ -28,11 +32,30 @@ class LandingPageHandler
         if ($urlParts[0] != FLUENT_BOOKING_LANDING_SLUG || count($urlParts) < 2) {
             return;
         }
-
         $authorSlug = sanitize_text_field($urlParts[1]);
 
-        $user = get_user_by('slug', $authorSlug);
+        $this->routeView($authorSlug, Arr::get($urlParts, 2, null));
+    }
 
+    public function handleUrlParamsPage()
+    {
+        if(empty($_REQUEST['host'])) {
+            return;
+        }
+        $authorSlug = sanitize_text_field($_REQUEST['host']);
+
+        $slotSlug = null;
+
+        if(!empty($_REQUEST['event'])) {
+            $slotSlug = sanitize_text_field($_REQUEST['event']);
+        }
+
+        $this->routeView($authorSlug, $slotSlug);
+    }
+
+    public function routeView($authorSlug, $slotSlug = null)
+    {
+        $user = get_user_by('slug', $authorSlug);
         if (!$user) {
             return;
         }
@@ -43,7 +66,17 @@ class LandingPageHandler
         if (!$calendar) {
             return;
         }
-        
+
+        if ($slotSlug) {
+            $slot = CalendarSlot::where('calendar_id', $calendar->id)
+                ->where('slug', $slotSlug)
+                ->first();
+            if (!$slot) {
+                return;
+            }
+            $this->renderBookingView($calendar, $slot);
+        }
+
         $this->renderCalendarView($calendar);
     }
 
@@ -86,23 +119,103 @@ class LandingPageHandler
             ],
         ];
 
+        $app = App::getInstance();
         status_header(200);
-        $this->render('author_landing', $data);
+        $app->view->render('landing.author_landing', $data);
         exit(200);
     }
 
-    public function handleUrlParamsPage()
+    private function renderBookingView($calendar, $slot)
     {
+        global $wp;
 
+        $slot->max_lookup_date = $slot->getMaxLookUpDate();
+        $slot->min_lookup_date = $slot->getMinLookUpDate();
+
+        if (!empty($_REQUEST['booking_id'])) {
+            $bookingHash = sanitize_text_field($_REQUEST['booking_id']);
+            $booking = Booking::where('hash', $bookingHash)
+                ->where('slot_id', $slot->id)
+                ->first();
+            if ($booking) {
+                $this->showBookingConfimationPage($booking, $slot);
+            }
+        }
+
+        $formFields = BookingService::getBookingFields($slot);
+
+        $authorProfile = $slot->getAuthorProfile(true);
+
+        $slot->location_settings = (object)[];
+        $slot->description = wpautop($slot->description);
+
+        $slot->pre_selects = false;
+
+        if (date('m') != date('m', strtotime($slot->min_lookup_date))) {
+            $slot->pre_selects = [
+                'month' => date('m', strtotime($slot->min_lookup_date)),
+                'year'  => date('Y', strtotime($slot->min_lookup_date))
+            ];
+        }
+
+        $data = [
+            'calendar'    => $calendar,
+            'slot'        => $slot,
+            'author'      => $authorProfile,
+            'title'       => $slot->title . ' with ' . $authorProfile['name'],
+            'description' => substr(strip_shortcodes(strip_tags(str_replace(PHP_EOL, ' ', $slot->description))), 0, 300) . '...',
+            'url'         => home_url($wp->request),
+            'css_files'   => [
+                App::getInstance('url.assets') . 'public/saas.css'
+            ],
+            'js_files'    => [
+                App::getInstance('url.assets') . 'public/js/app.js'
+            ],
+            'js_vars'     => [
+                'fcal_public_vars_' . $calendar->id . '_' . $slot->id => [
+                    'slot'           => $slot,
+                    'calendar'       => $calendar,
+                    'author_profile' => $authorProfile,
+                    'form_fields'    => $formFields
+                ],
+                'fluentCalendarPublicVars'                            => (new FrontEndHandler())->getGlobalVars()
+            ]
+        ];
+
+        $app = App::getInstance();
+
+        status_header(200);
+        $app->view->render('landing.booking', $data);
+        exit(200);
     }
 
-    private function render($file, $data)
-    {
 
+    private function showBookingConfimationPage($booking, $slot)
+    {
+        global $wp;
+        $responseHtml = BookingService::getBookingConfirmationHtml($booking, $slot, true);
+
+        $authorProfile = $slot->getAuthorProfile(true);
+
+        $data = [
+            'title'       => 'Confirmation: ' . $slot->title . ' with ' . $authorProfile['name'],
+            'body'        => $responseHtml,
+            'description' => substr(strip_shortcodes(strip_tags(str_replace(PHP_EOL, ' ', $slot->description))), 0, 300) . '...',
+            'css_files'   => [
+                App::getInstance('url.assets') . 'public/saas_public.css'
+            ],
+            'js_files'    => [],
+            'js_vars'     => [],
+            'author'      => $authorProfile,
+            'slot'        => $slot,
+            'url'         => home_url($wp->request),
+        ];
+
+        $app = App::getInstance();
+        status_header(200);
+        $app->view->render('landing.confirmation_page', $data);
+        exit(200);
     }
 
-    private function showErrorPage($title, $description = '')
-    {
-        die($title);
-    }
+
 }
---
+++ app/Views/landing/confirmation_page.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <title><?php echo esc_attr($title); ?></title>
+    <meta charset='utf-8'>
+
+    <meta content='width=device-width, initial-scale=1' name='viewport'>
+    <meta content='yes' name='apple-mobile-web-app-capable'>
+    <meta name="description" content="<?php echo esc_attr($description); ?>">
+    <meta name="robots" content="noindex">
+
+    <link rel="icon" type="image/x-icon" href="<?php echo $author['avatar']; ?>" />
+
+    <meta property="og:title" content="<?php echo esc_attr($title); ?>">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="<?php echo esc_url($url); ?>">
+    <meta property="og:site_name" content="ConvertLeap">
+    <meta property="og:description" content="<?php echo esc_attr($description); ?>">
+    <meta property="og:author" content="<?php echo $author['name']; ?>">
+
+    <meta property="og:image" content="<?php echo FLUENT_BOOKING_URL; ?>assets/images/default-featured.png" />
+
+
+    <?php foreach ($css_files as $css_file): ?>
+    <link rel="stylesheet" href="<?php echo $css_file; ?>" media="screen" />
+    <?php endforeach; ?>
+</head>
+<body>
+
+<div class="confirmation_page">
+    <div class="fcal_conf_wrap">
+        <?php echo $body; ?>
+    </div>
+</div>
+
+<script>
+    <?php foreach ($js_vars as $varKey => $values): ?>
+    var <?php echo $varKey; ?> = <?php echo json_encode($values); ?>;
+    <?php endforeach; ?>
+</script>
+
+<?php foreach ($js_files as $file): ?>
+    <script src="<?php echo $file; ?>" defer="defer"></script>
+<?php endforeach; ?>
+</body>
+</html>
