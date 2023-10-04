771942754cf96608f425c494663ba834ffb2784e
Shahjahan Jewel <cep.jewel@gmail.com>
2023-10-04 17:36:14 +0600

    Calendar Settings Revamped

---
+++ app/Services/LandingPage/LandingPageHelper.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace FluentBooking\App\Services\LandingPage;
+
+use FluentBooking\App\Models\Calendar;
+use FluentBooking\Framework\Support\Arr;
+
+class LandingPageHelper
+{
+    public static function getSettings(Calendar $calendar, $scope = 'admin')
+    {
+        $sharingSettings = $calendar->getMeta('sharing_settings', []);
+
+        $defaults = [
+            'enabled'       => 'no',
+            'enabled_slots' => [],
+            'show_type'     => 'all'
+        ];
+
+        $settings = wp_parse_args($sharingSettings, $defaults);
+        return apply_filters('fluent_booking/calendar/sharing_settings', $settings, $calendar, $scope);
+    }
+
+    public static function updateSettings(Calendar $calendar, $settings)
+    {
+        $defaults = [
+            'enabled'       => 'no',
+            'enabled_slots' => [],
+            'show_type'     => 'all'
+        ];
+
+        $sharingSettings = Arr::only($settings, array_keys($defaults));
+        $sharingSettings = wp_parse_args($sharingSettings, $defaults);
+
+        if ($sharingSettings['show_type'] == 'all' || $sharingSettings['enabled'] == 'no') {
+            $sharingSettings['enabled_slots'] = [];
+        } else {
+            $sharingSettings['enabled_slots'] = array_map('intval', $sharingSettings['enabled_slots']);
+        }
+
+        $sharingSettings['enabled'] = $sharingSettings['enabled'] == 'yes' ? 'yes' : 'no';
+
+        $settings['show_type'] = sanitize_text_field($sharingSettings['show_type']);
+
+        $calendar->updateMeta('sharing_settings', $sharingSettings);
+
+        return $settings;
+    }
+
+    public static function getLandingBaseUrl()
+    {
+        if (defined('FLUENT_BOOKING_LANDING_SLUG')) {
+            return site_url(FLUENT_BOOKING_LANDING_SLUG . '/');
+        }
+
+        return site_url('/?fluent-booking=calendar');
+    }
+}
---
+++ resources/admin/Modules/Calendars/integrations/LandingPageSettings.vue
@@ -0,0 +1,95 @@
+<template>
+    <div class="fcal_cal_settings">
+        <div class="fcal_settings_header">
+            <h2>Landing Page Settings</h2>
+            <p>Share your Booking Types in a beautiful & standalone landing page</p>
+        </div>
+        <div v-loading="loading" class="fcal_settings_body">
+            <el-form v-model="settings" label-position="top">
+                <el-form-item>
+                    <el-checkbox true-label="yes" false-label="no" v-model="settings.enabled">Enable Landing Page
+                        Features for this calendar
+                    </el-checkbox>
+                </el-form-item>
+                <el-form-item label="Landing Page Description">
+                    <el-input
+                        v-model="calendar.description"
+                        type="textarea"
+                        :rows="3"
+                        placeholder="Enter description for your landing page"
+                    />
+                </el-form-item>
+                <template v-if="settings.enabled == 'yes'">
+                    <el-form-item label="Which Booking Forms to Show?">
+                        <el-radio-group v-model="settings.show_type">
+                            <el-radio label="all">All Booking Forms</el-radio>
+                            <el-radio label="selected_only">Only Selected Active Booking Types</el-radio>
+                        </el-radio-group>
+                    </el-form-item>
+                    <el-form-item v-if="settings.show_type == 'selected_only'"
+                                  label="Please select which Booking Forms to show in the page?">
+                        <el-checkbox-group class="fcal_radio_lined" v-model="settings.enabled_slots">
+                            <el-checkbox v-for="slot in calendar.slots" :key="slot.id" :label="slot.id">{{
+                                    slot.title
+                                }}
+                            </el-checkbox>
+                        </el-checkbox-group>
+                    </el-form-item>
+                </template>
+                <el-form-item>
+                    <el-button @click="saveSettings()" :disabled="saving" v-loading="saving" type="primary">Save
+                        Settings
+                    </el-button>
+                </el-form-item>
+            </el-form>
+        </div>
+    </div>
+</template>
+
+<script type="text/babel">
+export default {
+    name: 'LandingPageCalendarSettings',
+    props: ['calendar'],
+    data() {
+        return {
+            loading: false,
+            settings: {},
+            saving: false
+        }
+    },
+    methods: {
+        fetchSettings() {
+            this.loading = true;
+            this.$get('calendars/' + this.calendar.id + '/sharing-settings')
+                .then(response => {
+                    this.settings = response.settings;
+                })
+                .catch(errors => {
+                    this.$handleError(errors);
+                })
+                .finally(() => {
+                    this.loading = false;
+                });
+        },
+        saveSettings() {
+            this.saving = true;
+            this.$post('calendars/' + this.calendar.id + '/sharing-settings', {
+                settings: this.settings,
+                description: this.calendar.description
+            })
+                .then(response => {
+                    this.$notify.success(response.message);
+                })
+                .catch(errors => {
+                    this.$handleError(errors);
+                })
+                .finally(() => {
+                    this.saving = false;
+                });
+        }
+    },
+    mounted() {
+        this.fetchSettings();
+    }
+}
+</script>
