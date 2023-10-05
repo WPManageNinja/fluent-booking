9c35eba920a9db79bb47367622df79198a1ac561
Tanbir <tanbirsylhet20@gmail.com>
2023-10-05 17:07:09 +0600

    Completed the FluentBooking block and added animations to the front-end page

--- resources/Blocks/attributes.js
+++ resources/Blocks/attributes.js
@@ -2,8 +2,16 @@
 const { __ } = wp.i18n;
 
 export const attributes = {
+    slotId: {
+        type: 'string',
+        default: ''
+    },
     calendarId: {
         type: 'string',
         default: ''
+    },
+    calendars: {
+        type: 'array',
+        default: []
     }
 };
