013776a2df19ad3aaccdd37699e2d00e23c3bf58
Shahjahan Jewel <cep.jewel@gmail.com>
2023-10-07 16:37:25 +0600

    added driver WIP

--- resources/admin/Modules/Calendars/integrations/RemoteCalendarsSettings.vue
+++ resources/admin/Modules/Calendars/integrations/RemoteCalendarsSettings.vue
@@ -7,19 +7,33 @@
         </div>
         <el-skeleton :rows="4" animated v-if="loading"/>
         <div v-else class="fcal_calendar_body">
-            <div class="fcal_hightlight_box fcal_create_event_selector">
-                <el-row :gutter="30">
-                    <el-col :md="16" :xs="24">
+            <div v-if="insertableCalendars.length" class="fcal_hightlight_box fcal_create_event_selector">
+                <el-row align="middle" :gutter="30">
+                    <el-col :md="14" :xs="24">
                         <h3>Create events on</h3>
                         <p>Select remote calendar in where to add new events to when you're booked.</p>
                     </el-col>
-                    <el-col :md="8" :xs="24">
-                        <pre>{{ insertableCalendars }}</pre>
+                    <el-col :md="10" :xs="24">
+                        <el-select clearable v-model="settings.create_event_calendar" value-key="id" placeholder="Select a Remote Calendar">
+                            <el-option
+                                v-for="item in insertableCalendars"
+                                :key="item.details.id"
+                                :label="item.title"
+                                :value="item.details"
+                            />
+                        </el-select>
                     </el-col>
                 </el-row>
             </div>
-            <div>
 
+            <div class="fcal_remote_calendars_blocks">
+                <div class="fcal_remote_calendar_block" v-for="feed in feeds" :key="db_id">
+                    <remote-calendar :feed="feed" :driver="providers[feed.driver]" :calendar="calendar" />
+                </div>
+            </div>
+
+            <div style="width: 800px; overflow: auto;">
+                <pre>{{ settings }}</pre>
                 <pre>{{ providers }}</pre>
                 <pre>{{ insertableCalendars }}</pre>
                 <pre>{{ feeds }}</pre>
@@ -31,15 +45,22 @@
 <script type="text/babel">
 import each from 'lodash/each';
 import isEmpty from 'lodash/isEmpty';
+import RemoteCalendar from './RemoteCalendar';
 
 export default {
     name: 'RemoteCalendarsSettings',
     props: ['calendar'],
+    components: {
+        RemoteCalendar
+    },
     data() {
         return {
             loading: false,
             providers: {},
-            feeds: []
+            feeds: [],
+            settings: {
+                create_event_calendar: ''
+            }
         }
     },
     computed: {
@@ -53,10 +74,12 @@ export default {
                 each(feed.remote_calendars, (item) => {
                     if (item.can_write === 'yes') {
                         calendars.push({
-                            id: item.id,
-                            driver: feed.driver,
-                            calendar_identifier: feed.identifier,
-                            title: item.title
+                            title: item.title + ' - ' + feed.driver + ' (' + feed.identifier + ')',
+                            id: feed.db_id + '__||__' + item.id,
+                            details: {
+                                id: feed.db_id + '__||__' + item.id,
+                                driver: feed.driver
+                            }
                         })
                     }
                 });
