1e7c1a73c1dac2d4494a195fb275a663f354c5ec
Tanbir <tanbirsylhet20@gmail.com>
2023-10-11 19:44:42 +0600

    Update BookingApp.svelte

--- resources/public/BookingApp.svelte
+++ resources/public/BookingApp.svelte
@@ -64,12 +64,10 @@
         component.parentNode.classList.remove("f_cal_day_selected");
         component.parentNode.classList.add("f_cal_spot_selected");
 
-        if (!isFluentform) {
-            const calendar = document.getElementsByClassName("fcal_calendar_inner")[0];
-            const height = calendarHeight + 135;
-            calendar.style.height = height + 'px';
-            selectedDate = spot;
-        }
+        const calendar = document.getElementsByClassName("fcal_calendar_inner")[0];
+        const height = calendarHeight + 135;
+        calendar.style.height = height + 'px';
+        selectedDate = spot;
     }
 
     function handleBookingConfirmation(confirmation) {
