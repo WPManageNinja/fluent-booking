9ee680b70f2531db89dabc687412e728670fc3e6
Shahjahan Jewel <cep.jewel@gmail.com>
2023-10-11 15:16:26 +0600

    added filters for vars

--- app/Hooks/Handlers/FrontEndHandler.php
+++ app/Hooks/Handlers/FrontEndHandler.php
@@ -60,13 +60,13 @@ class FrontEndHandler
 
         $slot->description = wpautop($slot->description);
 
-        wp_localize_script('fluent-booking-public', 'fcal_public_vars_' . $calendar->id . '_' . $slot->id, [
+        wp_localize_script('fluent-booking-public', 'fcal_public_vars_' . $calendar->id . '_' . $slot->id, apply_filters('fluent_calendar_public_event_vars', [
             'slot'           => $slot,
             'calendar'       => $calendar,
             'author_profile' => $slot->getAuthorProfile(true),
             'form_fields'    => $formFields,
             'disable_author' => $atts['disable_author'] == 'yes',
-        ]);
+        ], $slot, $calendar));
 
         return App::make('view')->make('public.calendar', [
             'slot'     => $slot,
