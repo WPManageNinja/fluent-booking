6efe5ee797a65d58ab27ebb089900ba8a893152f
Tanbir <tanbirsylhet20@gmail.com>
2023-10-12 14:53:16 +0600

    working on booking details

---
+++ resources/admin/Modules/Schedules/parts/PaymentLogs.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="fcal_schedule_event_infos">
+        <div class="fcal_schedule_event_infos_body">
+            <div class="fcal_schedule_details_header">
+                <h1 class="fcal_header_title">
+                    Payment History
+                </h1>
+            </div>
+            <div class="fcal_schedule_details_event">
+                <div class="fcal_schedule_details_event_item">
+                    <h3>Name</h3>
+                    <p>{{ booking.first_name }} {{ booking.last_name }}</p>
+                </div>
+                <div class="fcal_schedule_details_event_item">
+                    <h3>Email</h3>
+                    <p>{{ booking.email }}</p>
+                </div>
+            </div>
+
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>Product</th>
+                        <th>Qty</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Product Item</td>
+                        <td>1</td>
+                        <td>$44</td>
+                    </tr>
+                </tbody>
+                <tfoot>
+                    <tr></tr>
+                </tfoot>
+            </table>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PaymentLogs",
+    props: ['booking'],
+    data() {
+        return {
+            tableData: [
+                {
+                    date: '2016-05-03',
+                    name: 'Tom',
+                    address: 'No. 189, Grove St, Los Angeles',
+                },
+                {
+                    date: '2016-05-02',
+                    name: 'Tom',
+                    address: 'No. 189, Grove St, Los Angeles',
+                },
+                {
+                    date: '2016-05-04',
+                    name: 'Tom',
+                    address: 'No. 189, Grove St, Los Angeles',
+                },
+                {
+                    date: '2016-05-01',
+                    name: 'Tom',
+                    address: 'No. 189, Grove St, Los Angeles',
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
