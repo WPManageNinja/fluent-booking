d7807fc85dbc408bbb0c993cd09d50421ec6d705
Shahjahan Jewel <cep.jewel@gmail.com>
2023-10-12 15:57:07 +0600

    Email Editor Fixed

--- resources/admin/Modules/Calendars/Edit/_NotificationSettings.vue
+++ resources/admin/Modules/Calendars/Edit/_NotificationSettings.vue
@@ -14,39 +14,40 @@
             <div :class="['fcal_notification_container', {disabled: !notification.enabled}]"
                  v-for="(notification, index) in notifications" :key="index">
                 <div class="fcal_notification_header">
-                    <span :class="['header_left', {active: isEditOpen[index]}]">
+                    <span :class="['header_left']">
                         {{ notification.title }}
                     </span>
                     <div class="header_right">
-                        <span v-if="notification.enabled">
-                            <el-button @click="toggleEdit(index)" class="fcal_plain_btn">
+                        <span v-if="!notification.enabled" class="fcal_plain_btn disable"> Disabled </span>
+                        <span>
+                            <el-button @click="toggleEdit(notification)" class="fcal_plain_btn">
                                 <el-icon><EditPen/></el-icon> Edit
                             </el-button>
                         </span>
-                        <span v-else class="fcal_plain_btn disable"> Disabled </span>
-                        <el-switch v-model="notification.enabled" @click="closeEdit(index)"></el-switch>
+                        <el-switch v-model="notification.enabled" @change="saveSettings()"></el-switch>
                     </div>
                 </div>
-
-                <el-dialog
-                    v-model="isEditOpen[index]"
-                    v-if="notification.enabled"
-                    title="Edit Notification"
-                    class="fcal_modal fcal_notification_modal"
-                    :close-on-click-modal="false"
-                >
-                    <EditNotificationSettings :email="notification.email"/>
-                    <template #footer>
-                        <div class="dialog-footer">
-                            <el-button class="fcal_primary_btn" @click="saveSettings">Save Email</el-button>
-                        </div>
-                    </template>
-                </el-dialog>
             </div>
         </div>
         <div class="fcal_create_calendar_form_footer">
             <SaveButton :saving="saving" label="Save Changes" @save="saveSettings"/>
         </div>
+
+        <el-dialog
+            v-model="showEdit"
+            v-if="showEdit"
+            title="Edit Notification"
+            class="fcal_modal fcal_notification_modal"
+            :close-on-click-modal="false"
+        >
+            <EditNotificationSettings v-if="editingNotification.email" :email="editingNotification.email"/>
+            <template #footer>
+                <div class="dialog-footer">
+                    <el-button class="fcal_primary_btn" :disabled="saving" v-loading="saving" @click="saveSettings">Save Email</el-button>
+                </div>
+            </template>
+        </el-dialog>
+
     </div>
     <div v-else class="fcal_section_body">
         <el-skeleton :rows="1" animated/>
@@ -72,26 +73,26 @@ export default {
     },
     data() {
         return {
+            editingNotification: {},
+            showEdit: false,
             notifications: {},
             loading: false,
             saving: false,
-            isEditOpen: [],
             stepIndex: 3
         }
     },
-    computed: {
-        editButtonText() {
-            return (index) => this.isEditOpen[index] ? 'Close' : 'Edit';
-        },
-    },
     methods: {
-        toggleEdit(index) {
-            const value = this.isEditOpen[index];
-            this.isEditOpen = [];
-            this.isEditOpen[index] = !value;
+        toggleEdit(notification) {
+            this.showEdit = !this.showEdit;
+            if(this.showEdit) {
+                this.editingNotification = notification;
+            } else {
+                this.editingNotification = {};
+            }
         },
-        closeEdit(index) {
-            this.isEditOpen[index] = false;
+        closeEdit() {
+            this.showEdit = false;
+            this.editingNotification = {};
         },
         fetch() {
             this.loading = true;
@@ -109,13 +110,13 @@ export default {
                 });
         },
         saveSettings() {
-            this.isEditOpen = [];
             this.saving = true;
             this.$post('calendars/' + this.slot.calendar.id + '/slots/' + this.slot.id + '/notifications', {
                 notifications: this.notifications
             })
                 .then(response => {
                     this.$handleSuccess(response);
+                    this.closeEdit();
                 })
                 .catch(errors => {
                     this.$handleError(errors);
