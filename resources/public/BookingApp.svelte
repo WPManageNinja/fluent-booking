a5c19502f68a538f1eab871182362af41c5595e7
Hasanuzzaman <hasanuzzamanbe@gmail.com>
2023-10-13 16:20:07 +0600

    Back from Payment restricted

--- resources/public/BookingApp.svelte
+++ resources/public/BookingApp.svelte
@@ -66,7 +66,6 @@
 
 
     function onPaymentsVisibilityChanged(visibility) {
-        console.log(visibility)
         showingPayments = visibility;
     }
 
@@ -226,26 +225,20 @@
 
                                 <div class="fcal_date_event_details_header">
                                     <h2>
-                                        <div aria-label="Back to Date Selection" on:click={(e) => {
-
-                                            if (showingPayments) {
-                                                showingPayments = false;
-                                            }else{
-                                                resetSelection()
-                                            }
-
-                                             }} on:keypress={(e) => { selectedDate = false }} class="fcal_back">
-                                            <i class="fcal_svg">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
-                                                    <path fill="none" d="M0 0h24v24H0V0z"/>
-                                                    <path
-                                                        d="M19 11H7.83l4.88-4.88c.39-.39.39-1.03 0-1.42-.39-.39-1.02-.39-1.41 0l-6.59 6.59c-.39.39-.39 1.02 0 1.41l6.59 6.59c.39.39 1.02.39 1.41 0 .39-.39.39-1.02 0-1.41L7.83 13H19c.55 0 1-.45 1-1s-.45-1-1-1z"/>
-                                                </svg>
-                                            </i>
-                                        </div>
                                         {#if showingPayments}
                                             Payment Details
                                         {:else}
+                                            <div aria-label="Back to Date Selection" on:click={(e) => {
+                                                resetSelection()
+                                             }} on:keypress={(e) => { selectedDate = false }} class="fcal_back">
+                                                <i class="fcal_svg">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+                                                        <path fill="none" d="M0 0h24v24H0V0z"/>
+                                                        <path
+                                                                d="M19 11H7.83l4.88-4.88c.39-.39.39-1.03 0-1.42-.39-.39-1.02-.39-1.41 0l-6.59 6.59c-.39.39-.39 1.02 0 1.41l6.59 6.59c.39.39 1.02.39 1.41 0 .39-.39.39-1.02 0-1.41L7.83 13H19c.55 0 1-.45 1-1s-.45-1-1-1z"/>
+                                                    </svg>
+                                                </i>
+                                            </div>
                                             Enter Details
                                         {/if}
                                     </h2>
