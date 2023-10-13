61fce22d0294685d4026827cca2c4aaca866794c
Tanbir <tanbirsylhet20@gmail.com>
2023-10-13 16:26:46 +0600

    fixed some minor ui issues

--- resources/public/BookingApp.svelte
+++ resources/public/BookingApp.svelte
@@ -84,7 +84,6 @@
 
         setTimeout(() => {
             const currentHeight = document.querySelector(".fcal_date_event_details.is_active .fcal_booking_form_wrap").offsetHeight;
-            console.log(currentHeight);
             calendarHeight = currentHeight + 135;
             calendar.style.height = calendarHeight + 'px';
         }, 100);
