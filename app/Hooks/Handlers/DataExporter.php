eb320e197dd1cd41289a4dc7d1e9ea7f35c91c23
Shahjahan Jewel <cep.jewel@gmail.com>
2023-10-14 18:12:40 +0600

    Update DataExporter.php

--- app/Hooks/Handlers/DataExporter.php
+++ app/Hooks/Handlers/DataExporter.php
@@ -4,6 +4,7 @@ namespace FluentBooking\App\Hooks\Handlers;
 
 
 use FluentBooking\App\Models\Booking;
+use FluentBooking\App\Services\PermissionManager;
 use FluentBooking\Framework\Request\Request;
 
 class DataExporter
@@ -13,6 +14,10 @@ class DataExporter
 
     public function exportBookingHosts()
     {
+
+        if(!PermissionManager::hasAllCalendarAccess()) {
+            die('You do not have permission to export data');
+        }
 
         $groupId = (int)$_REQUEST['group_id'];
 
@@ -25,42 +30,47 @@ class DataExporter
         $attendeesData = [];
         foreach ($attendees as $attendee) {
 
-            $order = $attendee->payment_order->load(['items', 'transaction']);
+            if ($attendee->payment_status) {
+                $order = $attendee->payment_order;
+                if ($order) {
+                    $order->load(['items', 'transaction']);
+                    $attendeesData['payment_order'] = [
+                        'status'         => $order->status,
+                        'payment_method' => $order->payment_method,
+                        'currency'       => $order->currency,
+                        'total_amount'   => $order->total_amount / 100,
+                        'created_at'     => $order->created_at,
+                        'items'          => $order->items,
+                        'transaction'    => [
+                            'id'               => $order->transaction->id,
+                            'vendor_charge_id' => $order->transaction->vendor_charge_id,
+                            'payment_method'   => $order->transaction->payment_method,
+                            'status'           => $order->transaction->status,
+                            'total'            => $order->transaction->total / 100,
+                            'created_at'       => $order->transaction->created_at,
+                        ]
+                    ];
+                }
+            }
 
-            $attendeesData['payment_order']    = [
-                'status'       => $order->status,
-                'payment_method' => $order->payment_method,
-                'currency'       => $order->currency,
-                'total_amount'   => $order->total_amount / 100,
-                'created_at'     => $order->created_at,
-                'items'         => $order->items,
-                'transaction'          => [
-                    'vendor_charge_id' => $order->transaction->vendor_charge_id,
-                    'payment_method'   => $order->transaction->payment_method,
-                    'status'           => $order->transaction->status,
-                    'total'            => $order->transaction->total / 100,
-                    'created_at'       => $order->transaction->created_at,
-                ]
-            ];
 
-            $attendeesData['first_name']       = $attendee->first_name;
-            $attendeesData['last_name']        = $attendee->last_name;
-            $attendeesData['email']            = $attendee->email;
-            $attendeesData['message']          = $attendee->message;
+            $attendeesData['first_name'] = $attendee->first_name;
+            $attendeesData['last_name'] = $attendee->last_name;
+            $attendeesData['email'] = $attendee->email;
+            $attendeesData['message'] = $attendee->message;
             $attendeesData['location_details'] = $attendee->location_details;
-            $attendeesData['source']           = $attendee->source;
-            $attendeesData['booking_type']     = $attendee->booking_type;
-            $attendeesData['source_url']       = $attendee->source_url;
-            $attendeesData['start_time']       = $attendee->start_time;
-
+            $attendeesData['source'] = $attendee->source;
+            $attendeesData['booking_type'] = $attendee->booking_type;
+            $attendeesData['source_url'] = $attendee->source_url;
+            $attendeesData['start_time'] = $attendee->start_time;
         }
-        $attendeesData = apply_filters('fluent_booking/editing_booking_hosts_data', $attendeesData, $attendees);
+
+        $attendeesData = apply_filters('fluent_booking/exporting_booking_data', $attendeesData, $attendees);
 
         header('Content-disposition: attachment; filename=Booking-Event-Guests-' . $groupId . '.json');
         header('Content-type: application/json');
         echo wp_json_encode($attendeesData);
         exit();
-
     }
 
 }
