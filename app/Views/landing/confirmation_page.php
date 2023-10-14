3acbf6f62f5a28de91872a382ce3b871cbb08f87
Tanbir <tanbirsylhet20@gmail.com>
2023-10-14 12:17:47 +0600

    Featured image doesn’t show up fixed

--- app/Views/landing/confirmation_page.php
+++ app/Views/landing/confirmation_page.php
@@ -18,7 +18,16 @@
     <meta property="og:description" content="<?php echo esc_attr($description); ?>">
     <meta property="og:author" content="<?php echo $author['name']; ?>">
 
-    <meta property="og:image" content="<?php echo FLUENT_BOOKING_URL; ?>assets/images/default-featured.png" />
+
+    <?php
+    $feature_image = '';
+    if ($author['featured_image']) {
+        $feature_image = $author['featured_image'];
+    } else {
+        $feature_image = FLUENT_BOOKING_URL .'assets/images/default-featured.png';
+    }
+    ?>
+    <meta property="og:image" content="<?php echo $feature_image; ?>" />
 
 
     <?php foreach ($css_files as $css_file): ?>
