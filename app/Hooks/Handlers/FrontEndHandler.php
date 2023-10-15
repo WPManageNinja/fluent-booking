ee786a8c0617f07167f5d65e5aab0345b1aff719
Hasanuzzaman <hasanuzzamanbe@gmail.com>
2023-10-15 21:45:08 +0600

    update translatable

--- app/Hooks/Handlers/FrontEndHandler.php
+++ app/Hooks/Handlers/FrontEndHandler.php
@@ -141,7 +141,17 @@ class FrontEndHandler
             'ajaxurl'        => admin_url('admin-ajax.php'),
             'timezones'      => DateTimeHelper::getFlatGroupedTimeZones(),
             'current_person' => $currentPerson,
-            'start_day'      => $startDay
+            'start_day'      => $startDay,
+            'i18'            => [
+                'Timezone' => __('Timezone', 'fluent-booking'),
+                'minutes'  => __('minutes', 'fluent-booking'),
+                'Enter Details' => __('Enter Details', 'fluent-booking'),
+                'Payment Details' => __('Payment Details', 'fluent-booking'),
+                'Total Payment' => __('Total Payment', 'fluent-booking'),
+                'Pay Now'       => __('Pay Now', 'fluent-booking'),
+                'Confirm Payment' => __('Confirm Payment', 'fluent-booking'),
+                'processing' => __('Processing', 'fluent-booking'),
+            ]
         ];
     }
 
