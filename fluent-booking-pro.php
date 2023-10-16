7d4bfea20dd30a99d6566654354ac740cbe57fae
Shahjahan Jewel <cep.jewel@gmail.com>
2023-10-16 22:46:52 +0600

    Change Plugin File Name

--- fluent-booking-pro.php
+++ fluent-booking-pro.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: Fluent Booking
 Description: Fluent Booking WordPress Plugin
-Version: 0.3
+Version: 0.4
 Author: Meeting scheduling made easy
 Author URI: https://wpmanageninja.com
 Plugin URI: https://fluentbooking.com
@@ -11,12 +11,11 @@ License: GPLv2 or later
 Text Domain: fluent-booking
 Domain Path: /language
 */
-
 define('FLUENT_BOOKING_DIR', plugin_dir_path(__FILE__));
 define('FLUENT_BOOKING_URL', plugin_dir_url(__FILE__));
 define('FLUENT_BOOKING_ASSETS_VERSION', time());
 define('FLUENT_BOOKING_DIR_FILE', __FILE__);
-define('FLUENT_BOOKING_VERSION', '0.3');
+define('FLUENT_BOOKING_VERSION', '0.4');
 
 require __DIR__.'/vendor/autoload.php';
 
--- public-webpack.mix.js
+++ public-webpack.mix.js
@@ -28,17 +28,17 @@ mix.js('resources/public/fluentform.js', 'assets/public/js')
     })
     .options({autoprefixer: false});
 
-mix.js('resources/public/ExtendedPhone/phone-field.js', 'assets/public/js')
-    .svelte({
-        dev: true,
-        compilerOptions: {
-            dev: true,
-        }
-    })
-    .options({
-        autoprefixer: false,
-        processCssUrls: false
-    });
+// mix.js('resources/public/ExtendedPhone/phone-field.js', 'assets/public/js')
+//     .svelte({
+//         dev: true,
+//         compilerOptions: {
+//             dev: true,
+//         }
+//     })
+//     .options({
+//         autoprefixer: false,
+//         processCssUrls: false
+//     });
 
 mix.js('resources/public/public-manage-meeting.js', 'assets/public/js');
 
