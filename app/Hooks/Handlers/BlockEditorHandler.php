6ac1b34dd58309743edeacce86caf8c0a7e57d02
Tanbir <tanbirsylhet20@gmail.com>
2023-10-19 15:45:05 +0600

    added some customization options to gutenberg block

--- app/Hooks/Handlers/BlockEditorHandler.php
+++ app/Hooks/Handlers/BlockEditorHandler.php
@@ -35,6 +35,18 @@ class BlockEditorHandler
                 'avatar_rounded' => [
                     'type'      => 'boolean',
                     'default'   => false
+                ],
+                'primary_color' => [
+                    'type'      => 'string',
+                    'default'   => '#4587EC'
+                ],
+                'date_round' => [
+                    'type'      => 'string',
+                    'default'   => '4px'
+                ],
+                'avatarStyle' => [
+                    'type'      => 'string',
+                    'default'   => '8px'
                 ]
             ]
         ));
@@ -42,6 +54,20 @@ class BlockEditorHandler
 
     public function fcal_render_block($attributes)
     {
+        add_action('wp_head', function () use ($attributes) {
+            ?>
+            <style>
+                .fcal_calendar_inner .fcal_side .fcal_author_avatar img {
+                    border-radius: 50% !important;
+                }
+                :root {
+                    --fcal_primary_color: <?php echo esc_attr($attributes['primary_color']); ?> !important;
+                    --fcal_date_radius: <?php echo esc_attr($attributes['date_round']); ?> !important;
+                    --fcal_avatar_radius: <?php echo esc_attr($attributes['avatarStyle']); ?> !important;
+                }
+            </style>
+            <?php
+        });
         $slotId = $attributes['slotId'];
         return do_shortcode("[fluent_booking id=$slotId]");
     }
