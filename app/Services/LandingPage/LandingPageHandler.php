a6f76bb1c5a7098eb10f6f742f7b4255bfeecf44
Tanbir <tanbirsylhet20@gmail.com>
2023-10-20 20:56:19 +0600

    refactored codes

--- app/Services/LandingPage/LandingPageHandler.php
+++ app/Services/LandingPage/LandingPageHandler.php
@@ -179,8 +179,6 @@ class LandingPageHandler
 
         $eventVars = (new FrontEndHandler())->getCalendarEventVars($calendar, $calendarEvent);
 
-        $eventVars['time_format'] = get_option('_fluent_booking_settings')['time_format'];
-
         $data = [
             'calendar'       => $calendar,
             'calendar_event' => $calendarEvent,
