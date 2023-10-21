ca1cb45220d3667ff94da484ba63ed97fb006f06
Shahjahan Jewel <cep.jewel@gmail.com>
2023-10-21 18:02:29 +0600

    1.0.3 Release

--- app/Models/CalendarSlot.php
+++ app/Models/CalendarSlot.php
@@ -4,6 +4,7 @@ namespace FluentBooking\App\Models;
 
 use FluentBooking\App\Models\Model;
 use FluentBooking\App\Services\BookingFieldService;
+use FluentBooking\App\Services\DateTimeHelper;
 use FluentBooking\App\Services\Helper;
 use FluentBooking\App\Services\BookingService;
 use FluentBooking\App\Services\LandingPage\LandingPageHandler;
@@ -217,17 +218,22 @@ class CalendarSlot extends Model
     {
         $rangeType = Arr::get($this->settings, 'range_type', 'range_days');
 
-        if ($rangeType == 'range_indefinite') {
-            return $startDate;
-        }
-
         if ($rangeType == 'range_date_between') {
             $range = Arr::get($this->settings, 'range_date_between', []);
             if (is_array($range) && count(array_filter($range)) == 2) {
                 if (strtotime($range[0]) >= strtotime($startDate)) {
-                    return date('Y-m-d H:i:s', strtotime($range[0]));
+                    $startDate = date('Y-m-d H:i:s', strtotime($range[0]));
                 }
             }
+        }
+
+        $cutOutSeconds = $this->getCutoutSeconds();
+        $currentAuthorTimezoneDateTime = DateTimeHelper::convertToTimeZone(date('Y-m-d H:i:s'), 'UTC', $this->calendar->author_timezone);
+
+        $totalCutStamp = strtotime($currentAuthorTimezoneDateTime) + $cutOutSeconds;
+
+        if(strtotime($startDate) < $totalCutStamp) {
+            $startDate = date('Y-m-d H:i:s', $totalCutStamp);
         }
 
         return $startDate;
