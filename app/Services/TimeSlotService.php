3a2ef9138c005552907e1ff435f4430bd938ed13
Shahjahan Jewel <cep.jewel@gmail.com>
2023-10-23 12:35:09 +0600

    Added

--- app/Services/TimeSlotService.php
+++ app/Services/TimeSlotService.php
@@ -190,8 +190,8 @@ class TimeSlotService
     protected function getBookedSlots($dateRange, $toTimeZone = false, $bookingRequest = false)
     {
         if ($toTimeZone) {
-            $dateRange[0] = DateTimeHelper::convertToTimeZone($dateRange[0], $toTimeZone, 'UTC');
-            $dateRange[1] = DateTimeHelper::convertToTimeZone($dateRange[1], $toTimeZone, 'UTC');
+            $dateRange[0] = DateTimeHelper::convertToUtc($dateRange[0], $toTimeZone);
+            $dateRange[1] = DateTimeHelper::convertToUtc($dateRange[1], $toTimeZone);
         }
 
         $hostIds = $this->calendarSlot->getHostIds();
@@ -205,7 +205,6 @@ class TimeSlotService
             ->groupBy('group_id');
 
         $maxBooking = $this->calendarSlot->getMaxBookingPerSlot();
-
         $books = [];
 
         foreach ($bookings as $booking) {
