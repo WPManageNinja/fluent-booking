bfe6b5f9887bb0885d8ba2f233020dd81a72134b
Raiyan Marzan <raiyanmarzan.rm@gmail.com>
2023-10-23 21:47:11 +0600

    Updated dailyHook function

--- app/Hooks/Handlers/ActivationHandler.php
+++ app/Hooks/Handlers/ActivationHandler.php
@@ -28,12 +28,14 @@ class ActivationHandler
         $fiveMinutesHook = 'fluent_booking_five_minutes_tasks';
         as_schedule_recurring_action(time(), (60 * 5), $fiveMinutesHook, [], 'fluent-booking', true);
 
-        $dailyHook = 'fluent_booking/daily_tasks';
-        as_schedule_recurring_action( time(), (60 * 60 * 24), $dailyHook,[],'fluent-booking',true );
-
         $hourlyHook = 'fluent_booking_hourly_tasks';
         if (!wp_next_scheduled($hourlyHook)) {
             wp_schedule_event(time(), 'hourly', $hourlyHook);
+        }
+
+        $dailyHook = 'fluent_booking/daily_tasks';
+        if (!wp_next_scheduled($dailyHook)) {
+            wp_schedule_event(time(), 'daily', $dailyHook);
         }
     }
 }
