414ca2d750fe178e67623173e4b51d63513d872a
Shahjahan Jewel <cep.jewel@gmail.com>
2023-10-25 14:41:03 +0600

    Added Build for Free and Pro

--- fluent-booking-pro.php
+++ fluent-booking-pro.php
@@ -12,8 +12,12 @@ Text Domain: fluent-booking-pro
 Domain Path: /language
 */
 
-define('FLUENT_BOOKING_PRO_DIR_FILE', __FILE__);
+if (defined('FLUENT_BOOKING_VERSION')) {
+    return;
+}
+
 define('FLUENT_BOOKING_DIR', plugin_dir_path(__FILE__));
+define('FLUENT_BOOKING_PRO_DIR_FILE', __FILE__);
 define('FLUENT_BOOKING_URL', plugin_dir_url(__FILE__));
 define('FLUENT_BOOKING_VERSION', '1.0.5');
 define('FLUENT_BOOKING_ASSETS_VERSION', '1.0.5');
--- fluent-booking.php
+++ fluent-booking.php
@@ -2,22 +2,27 @@
 
 /*
 Plugin Name: Fluent Booking
-Description: Fluent Booking WordPress Plugin (Free Version)
+Description: Appointment Scheduling WordPress Plugin
 Version: 1.0.0
-Author: Meeting scheduling made easy
-Author URI: https://wpmanageninja.com
+Author: techjewel
+Author URI: https://jewel.im
 Plugin URI: https://fluentbooking.com
 License: GPLv2 or later
-Text Domain: fluent-booking-pro
+Text Domain: fluent-booking
 Domain Path: /language
 */
+
+if (defined('FLUENT_BOOKING_VERSION')) {
+    return;
+}
+
+define('FLUENT_BOOKING_VERSION', '1.0.0');
 define('FLUENT_BOOKING_DIR', plugin_dir_path(__FILE__));
 define('FLUENT_BOOKING_URL', plugin_dir_url(__FILE__));
-define('FLUENT_BOOKING_VERSION', '1.0.0');
 define('FLUENT_BOOKING_ASSETS_VERSION', '1.0.0');
 
-require __DIR__.'/vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
-call_user_func(function($bootstrap) {
+call_user_func(function ($bootstrap) {
     $bootstrap(__FILE__);
-}, require(__DIR__.'/boot/app.php'));
+}, require(__DIR__ . '/boot/fluent_app.php'));
