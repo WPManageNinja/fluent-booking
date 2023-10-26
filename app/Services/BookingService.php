4276cbbaa3dbd8c5e926ae2d3130dba4c19e763d
Shahjahan Jewel <cep.jewel@gmail.com>
2023-10-26 22:03:15 +0600

    Added

--- app/Services/BookingService.php
+++ app/Services/BookingService.php
@@ -63,7 +63,7 @@ class BookingService
         if(empty($data['location_details'])) {
             $data['location_details'] = LocationService::getLocationDetails($calendarSlot, [], []);
         }
-        
+
         $bookingData = Arr::only(wp_parse_args($data, $defaults), (new Booking())->getFillable());
 
         $event = Booking::select('group_id')
