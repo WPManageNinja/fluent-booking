84c65a4d5924b7d1cb566ab18e6ec21b3d1bcc3d
Raiyan Marzan <raiyanmarzan.rm@gmail.com>
2023-11-20 11:58:04 +0600

    Reverted booking limit from timeslotservice

--- app/Services/TimeSlotService.php
+++ app/Services/TimeSlotService.php
@@ -32,10 +32,6 @@ class TimeSlotService
         $daySlots = $this->getWeekDaySlots();
         $bookedSlots = $this->getBookedSlots([$fromDate, $toDate], $this->calendar->author_timezone, $bookingRequest);
 
-        $bookingFrequency = Arr::get($this->calendarSlot->settings, 'booking_frequency', []);
-
-        $bookingDuration = Arr::get($this->calendarSlot->settings, 'booking_duration', []);
-
         $timeStamp = DateTimeHelper::getTimestamp($this->calendar->author_timezone);
         $cutOutTimeStamp = $timeStamp + $this->calendarSlot->getCutoutSeconds();
 
@@ -67,10 +63,6 @@ class TimeSlotService
             }
             
             $currentBookedSlots = $bookedSlots[$date] ?? [];
-
-            if ($this->hasReachedMaxLimit($bookingFrequency, $bookingDuration, $currentBookedSlots)) {
-                continue;
-            }
             
             $isToday = $date === $todayDate;
             $validSlots = [];
@@ -401,27 +393,6 @@ class TimeSlotService
         }
 
         return $formattedSlots;
-    }
-
-    protected function hasReachedMaxLimit($bookingFrequency, $bookingDuration, $bookedSlots)
-    {
-        if (Arr::isTrue($bookingFrequency, 'enabled')) {
-            $booked = array_filter($bookedSlots, function ($bookedSlot) {
-                return $this->calendarSlot->id == $bookedSlot['event_id'];
-            });
-    
-            return count($booked) >= Arr::get($bookingFrequency, 'limits.0.value');
-        }
-
-        if (Arr::isTrue($bookingDuration, 'enabled')) {
-            $booked = array_filter($bookedSlots, function ($bookedSlot) {
-                return $this->calendarSlot->id == $bookedSlot['event_id'];
-            });
-    
-            return count($booked) * $this->calendarSlot->duration >= Arr::get($bookingDuration, 'limits.0.value');
-        }
-        
-        return false;
     }
 
     public function getAvailableSpots($startDate, $timeZone = 'utc')
