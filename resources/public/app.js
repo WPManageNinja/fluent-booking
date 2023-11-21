1acc6a06c4d0be532378c7d482b91a24d086f98b
Tanbir <tanbirsylhet20@gmail.com>
2023-11-21 16:32:54 +0600

    WIP - Dark mode

--- resources/public/app.js
+++ resources/public/app.js
@@ -86,3 +86,23 @@ if (calendarApps.length) {
         window.fluentCalBootApp(calendarApps[index]);
     });
 }
+
+// System Mode
+const runColorMode = (fn) => {
+    if (!window.matchMedia) {
+        return;
+    }
+    const query = window.matchMedia('(prefers-color-scheme: dark)');
+    fn(query.matches);
+    query.addEventListener('change', (event) => fn(event.matches));
+}
+
+runColorMode((isDarkMode) => {
+    if (isDarkMode) {
+        document.body.classList.add('dark-mode');
+        document.body.classList.remove('light-mode');
+    } else {
+        document.body.classList.add('light-mode');
+        document.body.classList.remove('dark-mode');
+    }
+})
