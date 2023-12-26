dfb14815bb802a409a40675baf8a9a8886e284a3
Raiyan Marzan <raiyanmarzan.rm@gmail.com>
2023-12-26 12:40:16 +0600

    Updated Event Details Validation

--- resources/admin/Modules/Calendars/Edit/_EventDetails.vue
+++ resources/admin/Modules/Calendars/Edit/_EventDetails.vue
@@ -328,10 +328,11 @@ export default {
             if (this.calendar_event.settings.multi_duration?.enabled) {
                 if (!this.calendar_event.settings.multi_duration?.available_durations?.length) {
                     this.$handleError(this.$t('Multiple Duration requires at least 1 option'));
+                    return false;
                 } else if (!this.calendar_event.settings.multi_duration?.default_duration) {
                     this.$handleError(this.$t('Default Duration is required'));
+                    return false;
                 }
-                return false;
             }
             for (const location of this.calendar_event.location_settings) {
                 if (!location.type) {
