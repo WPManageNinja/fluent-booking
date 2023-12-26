ad3c4b6afa6876b1cd1cbc79ff7edb8f78ba71e9
Raiyan Marzan <raiyanmarzan.rm@gmail.com>
2023-12-26 09:51:22 +0600

    Updated online joining url

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -264,7 +264,7 @@ class Booking extends Model
             $html = '<b>' . $platformLabels[$locationType] . '</b> ';
 
             if ($meetingLink = Arr::get($details, 'online_platform_link')) {
-                $html .= '<a target="_blank" href="' . esc_url($meetingLink) . '">' . __('Online Joining URL', 'fluent-booking-pro') . '</a>';
+                $html .= '<a target="_blank" href="' . esc_url($meetingLink) . '">' . __('Joining Meeting', 'fluent-booking-pro') . '</a>';
             }
 
             return $html;
