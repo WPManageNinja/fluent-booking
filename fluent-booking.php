57044c26bf44c5cf58567108265856d332b18420
Shahjahan Jewel <cep.jewel@gmail.com>
2024-01-25 13:13:49 +0600

    Added Free Version Support

--- fluent-booking.php
+++ fluent-booking.php
@@ -25,4 +25,4 @@ require __DIR__ . '/vendor/autoload.php';
 
 call_user_func(function ($bootstrap) {
     $bootstrap(__FILE__);
-}, require(__DIR__ . '/boot/fluent_app.php'));
+}, require(__DIR__ . '/boot/app.php'));
