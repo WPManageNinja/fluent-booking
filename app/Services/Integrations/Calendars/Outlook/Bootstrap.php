5d75478c8b48953199fa7d602943e302d46a3b44
Raiyan Marzan <raiyanmarzan.rm@gmail.com>
2024-01-31 16:55:26 +0600

    Removed integration events upon booking deletion

--- app/Services/Integrations/Calendars/Outlook/Bootstrap.php
+++ app/Services/Integrations/Calendars/Outlook/Bootstrap.php
@@ -96,6 +96,8 @@ class Bootstrap extends BaseCalendar
                 $calendar->generic_error = '<p style="color: red; margin:0;">' . __('Outlook Calendar API Error:', 'fluent-booking-pro') . ' ' . $error . '. <a href="' . Helper::getAppBaseUrl('calendars/' . $calendar->id . '/settings/remote-calendars') . '">' . __('Click Here to Review', 'fluent-booking-pro') . '</a></p>';
             }, 10, 2);
         });
+
+        add_action('fluent_booking/delete_booking_async_outlook', [$this, 'asyncDeleteEvent'], 10, 3);
     }
 
     public function getClientSettingsForView($settings)
@@ -707,6 +709,36 @@ class Bootstrap extends BaseCalendar
         return true;
     }
 
+    public function deleteEvent($config, Booking $booking)
+    {
+        if (!$this->isConfigured()) {
+            return false;
+        }
+
+        $bookingMeta = $booking->getMeta('__outlook_calendar_event');
+
+        if (!$bookingMeta || !($outlookEventId = Arr::get($bookingMeta, 'id'))) {
+            return false; // Nothing to delete as there is no previous response of this booking
+        }
+
+        as_enqueue_async_action('fluent_booking/delete_booking_async_' . $config['driver'], [
+            $booking->host_user_id,
+            $config['remote_calendar_id'],
+            $outlookEventId
+        ], 'fluent-booking');
+    }
+
+    public function asyncDeleteEvent($hostId, $remoteCalendarId, $outlookEventId)
+    {
+        $calendarApi = OutlookHelper::getApiClientByUserId($hostId, $remoteCalendarId);
+        if (!$calendarApi) {
+            return;
+        }
+
+        // Let's delete the event
+        $calendarApi->deleteEvent($outlookEventId);
+    }
+    
     public function authDisconnect($meta)
     {
         // Let's remove the cache first
