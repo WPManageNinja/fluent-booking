30e044aeb05156ab9b1afd174271d1bad964642a
Raiyan Marzan <raiyanmarzan.rm@gmail.com>
2024-02-02 09:53:03 +0600

    Updated Error Text UI

--- resources/public/Components/BookingForm.svelte
+++ resources/public/Components/BookingForm.svelte
@@ -148,6 +148,11 @@
         </form>
         {#if errors}
             <div class="fcal_errors">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-5 w-5 text-blue-900 dark:text-blue-200" data-testid="info" aria-hidden="true">
+                    <circle cx="12" cy="12" r="10"></circle>
+                    <line x1="12" x2="12" y1="16" y2="12"></line>
+                    <line x1="12" x2="12.01" y1="8" y2="8"></line>
+                </svg>
                 {@html errors}
             </div>
         {/if}
