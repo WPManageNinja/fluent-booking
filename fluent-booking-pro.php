7fdca5a7b403a7f6e080daddd11f12dc28d54fe6
Raiyan Marzan <raiyanmarzan.rm@gmail.com>
2024-02-09 01:51:54 +0600

    Release ready 1.2.61

--- fluent-booking-pro.php
+++ fluent-booking-pro.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Fluent Booking Pro
 Description: Fluent Booking WordPress Plugin
-Version: 1.2.60
+Version: 1.2.61
 Author: Meeting scheduling made easy
 Author URI: https://wpmanageninja.com
 Plugin URI: https://fluentbooking.com
@@ -18,8 +18,8 @@ if (defined('FLUENT_BOOKING_VERSION')) {
 define('FLUENT_BOOKING_DIR', plugin_dir_path(__FILE__));
 define('FLUENT_BOOKING_PRO_DIR_FILE', __FILE__);
 define('FLUENT_BOOKING_URL', plugin_dir_url(__FILE__));
-define('FLUENT_BOOKING_VERSION', '1.2.60');
-define('FLUENT_BOOKING_ASSETS_VERSION', '1.2.60');
+define('FLUENT_BOOKING_VERSION', '1.2.61');
+define('FLUENT_BOOKING_ASSETS_VERSION', '1.2.61');
 
 require __DIR__ . '/vendor/autoload.php';
 
