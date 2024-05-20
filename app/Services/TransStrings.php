1cbf7399c46dde092217019fefc86e5948967452
Raiyan Marzan <raiyanmarzan.rm@gmail.com>
2024-05-20 16:36:46 +0600

    Updated logged in user bookings responsiveness

--- app/Services/TransStrings.php
+++ app/Services/TransStrings.php
@@ -752,6 +752,9 @@ class TransStrings
             'Select Calendar Event'                                                            => __('Select Calendar Event', 'fluent-booking-pro'),
             'Use this number for sending sms notification'                                     => __('Use this number for sending sms notification', 'fluent-booking-pro'),
             'Help Message'                                                                     => __('Help Message', 'fluent-booking-pro'),
+            'Format'                                                                           => __('Format', 'fluent-booking-pro'),
+            'Select Format'                                                                    => __('Select Format', 'fluent-booking-pro'),
+            'The default date format will be applied if no format is chosen.'                  => __('The default date format will be applied if no format is chosen.', 'fluent-booking-pro'),
             'Need Pro Version'                                                                 => __('is not available with the free version. Please upgrade to pro to get all the advanced features.', 'fluent-booking-pro')
         );
     }
