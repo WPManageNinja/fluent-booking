90b285d7273001ded7d65cc98dcb63ccf90eb030
Raiyan <raiyanmarzan.rm@gmail.com>
2024-05-31 14:33:52 +0600

    Applied Redirection on Woo Booking
    Task: https://lounge.authlab.io/projects#/boards/4/tasks/6647-Woocommerce-payment-integrated

--- app/Models/CalendarSlot.php
+++ app/Models/CalendarSlot.php
@@ -786,50 +786,6 @@ class CalendarSlot extends Model
         return $productPrices;
     }
 
-    public function getRedirectUrlWithQuery($booking)
-    {
-        $isEnabled     = Arr::isTrue($this->settings, 'custom_redirect.enabled');
-        $redirectUrl   = Arr::get($this->settings, 'custom_redirect.redirect_url', '');
-        $queryString   = Arr::get($this->settings, 'custom_redirect.query_string', '');
-        $isQueryString = Arr::get($this->settings, 'custom_redirect.is_query_string', 'no') == 'yes';
-
-        if ($isQueryString && $queryString) {
-            if (strpos($redirectUrl, '?')) {
-                $redirectUrl .= '&' . $queryString;
-            } else {
-                $redirectUrl .= '?' . $queryString;
-            }
-        }
-
-        if (!$isEnabled || empty($redirectUrl)) {
-            return '';
-        }
-
-        $redirectUrl = EditorShortCodeParser::parse($redirectUrl, $booking);
-
-        $isUrlParser = apply_filters('fluent_booking/will_parse_redirect_url_value', true, $this);
-
-        if ($isUrlParser) {
-            if (strpos($redirectUrl, '=&') || '=' == substr($redirectUrl, -1)) {
-                $urlArray    = explode('?', $redirectUrl);
-                $baseUrl     = array_shift($urlArray);
-                $query       = wp_parse_url($redirectUrl)['query'];
-                $queryParams = explode('&', $query);
-
-                $params = [];
-                foreach ($queryParams as $queryParam) {
-                    $paramArray = explode('=', $queryParam);
-                    if (!empty($paramArray[1])) {
-                        $params[$paramArray[0]] = $paramArray[1];
-                    }
-                }
-                $redirectUrl = add_query_arg($params, $baseUrl);
-            }
-        }
-
-        return $redirectUrl;
-    }
-
     public function isTeamDefaultSchedule()
     {
         if ($this->isTeamEvent()) {
