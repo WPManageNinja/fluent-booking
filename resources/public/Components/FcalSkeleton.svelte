5cc44b75091f89920b16e16f34493241b761fa4e
Raiyan <raiyanmarzan.rm@gmail.com>
2024-07-12 02:44:24 +0600

    Added Pre Select Time

--- resources/public/Components/FcalSkeleton.svelte
+++ resources/public/Components/FcalSkeleton.svelte
@@ -1,9 +1,9 @@
 <script>
-    export let items = 5;
+    export let rows = 5;
 </script>
   
 <div class="fcal_skeleton">
-    {#each Array(items) as _}
+    {#each Array(rows) as _}
        <div class="fcal_skeleton_item"></div>
     {/each}
 </div>
