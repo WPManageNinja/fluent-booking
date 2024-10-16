c5b2b7f7e26dafa30b598dab15fa07bbd0b9ea02
Raiyan <raiyanmarzan.rm@gmail.com>
2024-10-16 14:57:13 +0600

    Added Blocksy Elementor Support

--- app/Services/Integrations/Elementor/fcal-custom-elementor.js
+++ app/Services/Integrations/Elementor/fcal-custom-elementor.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     elementor.hooks.addAction('panel/open_editor/widget', (panel, model, view) => {
 
-        const controlContainer = document.querySelector('.elementor-control-selected_cal_id select');
+        const calendarContainer = document.querySelector('.elementor-control-selected_cal_id select');
 
         let calId = '';
-        if (controlContainer) {
-            calId = controlContainer.value;
+        if (calendarContainer) {
+            calId = calendarContainer.value;
             fetchEvents().catch(error => console.error('Initial fetch error:', error));
 
-            controlContainer.addEventListener('change', async (event) => {
+            calendarContainer.addEventListener('change', async (event) => {
                 calId = event.target.value;
                 try {
                     await fetchEvents();
@@ -38,16 +38,11 @@ document.addEventListener('DOMContentLoaded', () => {
                 const result = await response.json();
 
                 if (result.success) {
-                    // Process the events
-                    console.log(result.data);
-
                     const eventControl = document.querySelector('.elementor-control-selected_event_ids select');
 
                     if (eventControl) {
-                        // Clear existing options
                         eventControl.innerHTML = '';
 
-                        // Add new options
                         Object.entries(result.data).forEach(([key, value]) => {
                             const option = document.createElement('option');
                             option.value = key;
@@ -60,6 +55,45 @@ document.addEventListener('DOMContentLoaded', () => {
                 }
             } catch (error) {
                 console.error('Error fetching events:', error);
+            }
+        }
+
+        const eventControl = document.querySelector('.elementor-control-selected_event select');
+
+        if (eventControl) {
+            eventControl.addEventListener('change', async (event) => {
+                const eventId = event.target.value;
+                const eventHash = await fetchEventHash(eventId);
+
+                const controlView = elementor?.getPanelView()?.getCurrentPageView()?.getControlViewByName('event_hash');
+                if (controlView) {
+                    controlView.setValue(eventHash);
+                    controlView.triggerMethod('input:change');
+                } else {
+                    console.error('Event hash control not found');
+                }
+            });
+        }
+
+        async function fetchEventHash(eventId) {
+            const response = await fetch(window.fcal_elementor_ajax_object.ajaxurl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+                },
+                body: new URLSearchParams({
+                    action: 'get_event_hash',
+                    event_id: eventId,
+                    security: window.fcal_elementor_ajax_object.nonce
+                })
+            });
+
+            const result = await response.json();
+
+            if (result.success) {
+                return result.data.hash;
+            } else {
+                console.error(result.data.message);
             }
         }
     });
