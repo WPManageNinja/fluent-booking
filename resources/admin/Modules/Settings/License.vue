557a54840570d894d1cc49932a8b4b34025b3dec
Raiyan <raiyanmarzan.rm@gmail.com>
2025-08-26 19:31:49 +0600

    Added Booking UTM Source Tracking

--- resources/admin/Modules/Settings/License.vue
+++ resources/admin/Modules/Settings/License.vue
@@ -82,7 +82,7 @@ export default {
     },
     data() {
         return {
-            fetching: true,
+            fetching: false,
             verifying: false,
             licenseData: {},
             licenseKey: '',
